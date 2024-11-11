3c06ab8d62921cd7f4704e49183cbb235f265650
Madhuk0009 <madhuk0009@gmail.com>
2024-11-11 16:51:49 +0530

    Added observables

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,10 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+
+  madhuobvdata$ = new BehaviorSubject("madhu");
+
+  madhudata$ = new Subject();
 
   booksurl:string= "http://localhost:3000/BOOKS";
 
